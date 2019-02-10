83304b14fd773b90340b6bed5617d143fe6caa9b
Madina <madina@MacBook-Pro-Alimzhan.local>
2019-02-10 23:35:02 +0600

    Responsive design

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,15 @@
-var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    spaceBetween: 10,
+(function() {
 
-    // If we need pagination
-    pagination: {
-    el: '.swiper-pagination',
-    },
+    'use strict';
+    
+    function init() {
+        document.querySelector("#menuToggle").addEventListener("click", function() {
+            document.getElementById("nav").classList.toggle("nav--openned");
+            document.getElementById("main").classList.toggle("main--hidden");
+            document.getElementById("cross").classList.toggle("cross--displayed");
+            document.getElementById("hamburger").classList.toggle("hamburger--hidden");
+        });
+    };
 
-    // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    }
-});
-
-function toggleMenu() {
-    document.getElementById("nav").classList.toggle("nav--openned");
-    document.getElementById("main").classList.toggle("main--hidden");
-    document.getElementById("cross").classList.toggle("cross--displayed");
-    document.getElementById("hamburger").classList.toggle("hamburger--hidden");
-};
-
-document.querySelector("#toggleSiteMode").addEventListener("change", function() {
-    document.getElementById("body").classList.toggle("night");
-});
-
-[].forEach.call(document.querySelectorAll(".nav-a "),function(link) {
-   link.addEventListener("click", function() {toggleMenu();});
-});
+    init();
+  })();
---
+++ scripts/swiperHelper.js
@@ -0,0 +1,36 @@
+(function() {
+
+    'use strict';
+  
+    const breakpoint = window.matchMedia( '(min-width:60em)' );
+  
+    let mySwiper;
+  
+    const breakpointChecker = function() {
+  
+      if ( breakpoint.matches === false ) {
+        if ( mySwiper !== undefined ) 
+            mySwiper.destroy( true, true );
+            return;
+        } else if ( breakpoint.matches === true ) {
+          return enableSwiper();
+        }
+    };
+  
+    const enableSwiper = function() {
+  
+     mySwiper = new Swiper('.swiper-container', {
+        direction: 'vertical',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        loop: true
+    });
+  
+    };
+  
+    breakpoint.addListener(breakpointChecker);
+  
+    breakpointChecker();
+  })();
