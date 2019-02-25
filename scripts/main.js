b28fe6525fed6459564187c127ebfb7cfe5ee539
Madina <madina@MacBook-Pro-Alimzhan.local>
2019-02-25 11:21:48 +0600

    fixes

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,73 @@
 (function() {
 
     'use strict';
+
+    let main = document.getElementById("main");
     
     function init() {
-        document.querySelector("#menuToggle").addEventListener("click", function() {
-            document.getElementById("nav").classList.toggle("nav--openned");
-            document.getElementById("main").classList.toggle("main--hidden");
-            document.getElementById("cross").classList.toggle("cross--displayed");
-            document.getElementById("hamburger").classList.toggle("hamburger--hidden");
+        toggleMode();
+        clickOnButton();
+        clickOnLinks();
+    
+        document.getElementById("home-link").addEventListener("click", function() {
+            if(!main.classList.contains("main--fixed")) main.classList.add("main--fixed");
+            goToSection("home");
+        });
+
+        document.getElementById("about-link").addEventListener("click", function() {
+            if(!main.classList.contains("main--fixed")) main.classList.remove("main--fixed");
+            goToSection("about");
         });
     };
+
+    function toggleMode () {
+        document.querySelector("#toggleSiteMode").addEventListener("click", function() {
+            document.getElementById("body").classList.toggle("dark");
+        });
+    }
+
+    function clickOnButton () {
+        document.querySelector("#menuToggle").addEventListener("click", function() {
+            toggleMenu();
+        });
+    }
+
+    function clickOnLinks () {
+        document.querySelectorAll("#nav a").forEach(function(el) {
+            el.addEventListener("click", function () {
+                if(el.classList.contains("link")) {
+                    if (el.getAttribute("data-section") !== "home" 
+                    && el.getAttribute("data-section") !== "about"
+                    && main.classList.contains("main--fixed")) {
+                        main.classList.remove("main--fixed");
+                    }
+                    goToSection(el.getAttribute("data-section"));
+                } 
+
+                toggleMenu();
+            });
+        });
+    }
+
+    function toggleMenu () {
+        document.getElementById("nav").classList.toggle("nav--openned");
+        document.getElementById("main").classList.toggle("main--hidden");
+        document.getElementById("cross").classList.toggle("cross--displayed");
+        document.getElementById("hamburger").classList.toggle("hamburger--hidden");
+    }
+
+    function goToSection(section) {
+        document.querySelector(".section--active").classList.remove("section--active");
+        document.querySelector(".section--" + section).classList.add("section--active");
+    }
+
+    function show (el, style) {
+        el.style.display = style;
+    }
+
+    function hide (el) {
+        el.style.display = "none";
+    }
 
     init();
   })();
--- scripts/swiperHelper.js
+++ scripts/swiperHelper.js
@@ -19,21 +19,36 @@
   
     const enableSwiper = function() {
   
-     mySwiper = new Swiper('.swiper-container', {
+     mySwiper = new Swiper('.section--home', {
         direction: 'vertical',
         pagination: {
-            el: '.swiper-pagination',
+            el: '.swiper-pagination--home',
             clickable: true
         },
         loop: false,
         mousewheel: true,
         slidesPerView: 1,
-        speed: 1000
+        speed: 700
     });
   
     };
+
+    const initMassMediaSlider = function () {
+        new Swiper ('.swiper-container--mm', {
+            loop: true,
+            slidesPerView: 1,
+            pagination: {
+                el: '.swiper-container--mm .swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-container--mm .swiper-button-next',
+                prevEl: '.swiper-container--mm .swiper-button-prev',
+            }
+        });
+    }
   
     breakpoint.addListener(breakpointChecker);
   
     breakpointChecker();
+    //initMassMediaSlider();
   })();
